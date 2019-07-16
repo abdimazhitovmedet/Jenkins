d298eee259523b8fc9c335596a33b8f4ed49263d
medet <medetabdimazhitov@gmail.com>
2019-07-15 20:23:49 -0500

    first commit

---
+++ src/test/java/Jenkins.java
@@ -0,0 +1,12 @@
+public class Jenkins {
+
+
+    public static void main(String[] args) {
+
+
+        for (int i =0; i<100; i++){
+            System.out.println("I am perfect in Jenkins");
+
+        }
+    }
+}
